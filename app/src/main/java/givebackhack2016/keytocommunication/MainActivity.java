b2ad5de2aa68c04a53ecfeb1101753aaf2bd5070
buckeye1038 <haviland.10@osu.edu>
2016-02-20 13:07:45 -0500

    Initial Project

---
+++ app/src/main/java/givebackhack2016/keytocommunication/MainActivity.java
@@ -0,0 +1,25 @@
+package givebackhack2016.keytocommunication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this);
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+    }
+}
